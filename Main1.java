c181c4044b0bff997bb88b28ec9d23da0b27790b
qciap <tomasz.kuciapski@gmail.com>
2018-05-04 20:44:49 +0200

    my first repository

---
+++ Main1.java
@@ -0,0 +1,8 @@
+public class Main1{
+	public static void main(String[] args){
+	//zmienna referencyjna = obiekt person - to sie tyczy tego dolu
+	Person person = new Person();
+	person.setNameLastNameAge("Dariusz", "Bomba", 55);
+	person.printOutAllVariable();
+	}
+}
