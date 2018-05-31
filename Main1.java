80d12db4cb5cc35f69ef7d1a4615c81e8e642e4f
qciap <38156232+qciap@users.noreply.github.com>
2018-05-31 12:14:04 +0200

    Delete Main1.java

--- Main1.java
+++
@@ -1,8 +0,0 @@
-public class Main1{
-	public static void main(String[] args){
-	//zmienna referencyjna = obiekt person - to sie tyczy tego dolu
-	Person person = new Person();
-	person.setNameLastNameAge("Dariusz", "Bomba", 55);
-	person.printOutAllVariable();
-	}
-}
